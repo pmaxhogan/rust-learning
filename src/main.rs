7e73531007291afebcd99b3410f397635287ceeb
realprogrammer5000.com <me@programmer5000.com>
2020-09-24 14:14:42 -0400

    Make it do 1024-bit numbers

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
+use uint::construct_uint;
+// U1024 with 1024 bits consisting of 16 x 64-bit words
+construct_uint! {
+	pub struct u1024(16);
+}
+
 // https://en.wikipedia.org/wiki/Collatz_conjecture
 // returns the nth hailstone number
-// u32 gives the best performance for the sizes of numbers that we can compute in a reasonable amount
-// of time (tested with benchmark)
-// this means that the highest number we can return or take as input is 4294967295
-fn hailstone (number : u128) -> u128 {
+fn hailstone (number : u1024) -> u1024 {
     print!("{} ", number);
 
-    return if number == 1 {
-        1
-    } else if number % 2 == 0 {
+    return if number == u1024::from(1) {
+        u1024::from(1)
+    } else if number % 2 == u1024::from(0) {
         hailstone(number / 2)
     } else {
-        hailstone(3 * number + 1)
+        hailstone(u1024::from(3) * number + u1024::from(1))
     }
 }
 
 fn main() {
     println!("Hailstone sequence");
 
-    // calculate the hailstone sequence (?) for the highest unsigned 128-bit number
-    hailstone(u128::max_value());
+    // calculate the hailstone sequence (?) for a really high number
+    // this number is *really* big, 308 digits in total
+    hailstone(u1024::max_value() / u1024::from(5));
 
     print!("");
 }
+
