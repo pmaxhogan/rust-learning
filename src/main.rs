ecc9b0f0b0301d1e962cced47fa38d2867c219a6
realprogrammer5000.com <me@programmer5000.com>
2020-10-28 00:09:50 -0400

    Add lots of comments

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
+// termion is used for some raw terminal control things
+// it helps to improve portability
 extern crate termion;
 
 use std::io::{stdin, Write};
-use std::ops::Sub;
 use std::sync::mpsc;
 use std::thread;
 use std::thread::sleep;
-// we need both Duration and Instant from std::time
 use std::time::{Duration, Instant};
 
+// RNG for obstacle heights
 use rand::Rng;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 50;
-const ANIMATION_SPEED: usize = 90;
-const INITIAL_SPACING: usize = 40;
-const FPS: usize = 60;
-const AUTO_RESPAWN: bool = true;
+// constant variables (inlined by the compiler)
+const WIDTH: usize = 100;// the width of the "screen" in terminal characters
+const HEIGHT: usize = 50;// the height of the "screen" in terminal characters
+const ANIMATION_SPEED: usize = 90;// how fast the death animation is
+const INITIAL_SPACING: usize = 50;// how far away the first obstacle is
+const FPS: usize = 60;// screen (and physics!) updates per second
+const AUTO_RESPAWN: bool = true;// respawn after death animation
 
+// keyboard events
+// we need PartialEq to compare values
 #[derive(PartialEq, Copy, Clone)]
 enum KeyEvent {
     Jump,
@@ -43,29 +47,32 @@ enum GameState {
 struct State {
     player: Player,
     obstacles: Vec<Obstacle>,
-    jump_size: usize,
-    gap: usize,
-    spacing: usize,
+    jump_size: usize,// how high a jump is
+    gap: usize,// vertical gap between obstacles
+    spacing: usize,// horizontal spacing between obstacles
     game_state: GameState,
-    dead_timer: usize,
+    dead_timer: usize,// death animation timer
     score: usize,
 }
 
 struct Player {
     y_pos: usize,
     x_pos: usize,
-    jump_left: usize,
-    fall_speed: f32,
+    jump_left: usize,// how much jump is left
+    fall_speed: f32,// how fast the player is falling
 }
 
-// we need PartialEq to compare values
+// the "pixels" the display is made out of
 // Copy and Clone to be able to use the stack for storage (faster)
 #[derive(PartialEq, Copy, Clone)]
 enum Pixel {
+    // a predefined type,
     Empty,
     Vertical,
     Full,
-    FullAlt,
+    FullAlt,// alternate "full" character
+
+    // or a custom character
     Char(char),
 }
 
@@ -79,17 +86,21 @@ fn gen_spiral_vector(width: usize, height: usize) -> Vec<(usize, usize)> {
 fn gen_spiral_vector_with_offset(x_offset: usize, y_offset: usize, width: usize, height: usize) -> Vec<(usize, usize)> {
     let mut v: Vec<(usize, usize)> = Vec::new();
 
+    // draw downwards
     for y in 0..height {
         v.push((x_offset, y + y_offset));
     }
 
     if width > 1 {
+        // to the right
         for x in 1..width {
             v.push((x + x_offset, height - 1 + y_offset));
         }
+        // up
         for y in (0..(height - 1)).rev() {
             v.push((width - 1 + x_offset, y + y_offset));
         }
+        // and to the left
         if height > 1 {
             for x in (1..(width - 1)).rev() {
                 v.push((x + x_offset, y_offset));
@@ -97,6 +108,7 @@ fn gen_spiral_vector_with_offset(x_offset: usize, y_offset: usize, width: usize,
         }
     }
 
+    // draw the next ring if needed
     if width > 2 && height > 2 {
         v.append(&mut gen_spiral_vector_with_offset(1 + x_offset, 1 + y_offset, width - 2, height - 2));
     }
@@ -108,10 +120,11 @@ fn clear_terminal_and_reset_cursor() {
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 }
 
+// empties the display
 fn initialize_display(display: &mut [[Pixel; HEIGHT]; WIDTH]) {
     for y in 0..display[0].len() {
         for x in 0..display.len() {
-            display[x][y] = if y == HEIGHT { Pixel::Full } else { Pixel::Empty }
+            display[x][y] = Pixel::Empty;
         }
     }
 }
@@ -165,20 +178,26 @@ fn draw_display(display: &mut [[Pixel; HEIGHT]; WIDTH], mut state: &mut State) {
     }
 }
 
+// handles movement, jumping, falling, collision, and score
 fn physics(mut state: &mut State, tick: i32) {
     let mut passed_obstacle = false;
+
+    // for each obstacle,
     for obstacle in &state.obstacles {
+        // if this obstacle is in the same vertical column as the player,
         if state.player.x_pos == obstacle.x {
+            // check if they collide on any y value
             for y in obstacle.y..(obstacle.y + obstacle.height) {
                 if state.player.y_pos == y {
+                    // dead!
                     state.game_state = GameState::DeathAnimation;
                     return;
                 }
             }
+
             // we're still here?
             // it means that we were in the vertical area of an obstacle, yet didn't hit it
             // we need to ensure that we passed the other obstacle before increasing the score
-
             if passed_obstacle {
                 state.score += 1;
             } else {
@@ -187,16 +206,19 @@ fn physics(mut state: &mut State, tick: i32) {
         }
     }
 
-    if tick % 2 == 0 {
-        if state.player.jump_left > 0 {
-            if state.player.y_pos > 0 {
-                state.player.y_pos -= 1;
-            }
-            state.player.jump_left -= 1;
+    // every other tick, run the jump logic (prevents jumps from being too fast)
+    if tick % 2 == 0 && state.player.jump_left > 0 {
+        // if we can go up (not at the top of the screen)
+        if state.player.y_pos > 0 {
+            // go up
+            state.player.y_pos -= 1;
         }
+        state.player.jump_left -= 1;
     }
 
+    // every 10 ticks, move the screen forward, and make the player fall
     if tick % 10 == 0 {
+        // get a integer value (because the player position must be an integer
         let fall_height = state.player.fall_speed.round() as usize;
         if state.player.y_pos + 1 < HEIGHT && state.player.jump_left == 0 {
             // ensure that we don't go further than the bottom
@@ -206,8 +228,10 @@ fn physics(mut state: &mut State, tick: i32) {
             state.player.fall_speed += 0.3;
         }
 
+        // discard objects that have moved off the screen
         state.obstacles.retain(|obstacle| &obstacle.x > &0);
 
+        // find the furthest away obstacle
         let mut highest_x = 0;
         for idx in 0..state.obstacles.len() {
             let obstacle = &mut state.obstacles[idx];
@@ -222,14 +246,20 @@ fn physics(mut state: &mut State, tick: i32) {
     }
 }
 
+// "renders" the display by writing it to the terminal
 fn render_display(display: &[[Pixel; HEIGHT]; WIDTH]) {
+    // draw the top border
+    // note: \r is needed in terminal raw mode to reset the x-position of the cursor
     println!("{}", "@".repeat(WIDTH + 2) + "\r");
 
     // we use y for the outer to allow us to do display[x][y] instead of display[y][x]
     for y in 0..display[0].len() {
+        // draw the left border
         print!("@");
         for x in 0..display.len() {
             let elem = display[x][y];
+
+            // draw the pixels depending on what they are
             match elem {
                 Pixel::Empty => {
                     print!(" ");
@@ -248,13 +278,18 @@ fn render_display(display: &[[Pixel; HEIGHT]; WIDTH]) {
                 }
             }
         }
+        // draw the right border
         println!("@\r");
     }
 
+    // draw the bottom border
     println!("{}", "@".repeat(WIDTH + 2) + "\r");
 }
 
+// make (return) a pair of obstacles at the given x position
+// requires the state to know the gap
 fn make_obstacle_pair(state: &State, x: usize) -> (Obstacle, Obstacle) {
+    // what height the top obstacle ends
     let split_height = rand::thread_rng().gen_range(0, HEIGHT - state.gap);
     (Obstacle {
         x,
@@ -268,18 +303,19 @@ fn make_obstacle_pair(state: &State, x: usize) -> (Obstacle, Obstacle) {
      })
 }
 
+// add a pair of obstacles to the state's obstacle list at the provided x position
 fn add_obstacle_pair(state: &mut State, x: usize) {
     let pair = make_obstacle_pair(&state, x);
     state.obstacles.push(pair.0);
     state.obstacles.push(pair.1);
 }
 
-// sets up the display, state and tick variables
-fn setup() -> ([[Pixel; 50]; 100], State, i32) {
-    let mut display = [[Pixel::Empty; HEIGHT]; WIDTH];
+// sets up the display and state variables
+fn setup() -> ([[Pixel; 50]; 100], State) {
+    let display = [[Pixel::Empty; HEIGHT]; WIDTH];
     let mut state = State {
         player: Player {
-            x_pos: 3,
+            x_pos: 15,
             y_pos: 0,
             jump_left: 0,
             fall_speed: 1f32,
@@ -293,10 +329,7 @@ fn setup() -> ([[Pixel; 50]; 100], State, i32) {
         score: 0,
     };
 
-    for x in 0..display.len() {
-        display[x][display[0].len() - 1] = Pixel::Full;
-    }
-
+    // find out how many obstacles that we need to draw initially
     let num_obstacles = (WIDTH as f32 / state.spacing as f32).floor() as usize;
     for x in 0..num_obstacles {
         // we need to pull this var out because state is mutably borrowed below
@@ -304,55 +337,67 @@ fn setup() -> ([[Pixel; 50]; 100], State, i32) {
         add_obstacle_pair(&mut state, x * spacing + INITIAL_SPACING);
     }
 
-    let tick = 0;
-
-    (display, state, tick)
+    (display, state)
 }
 
 fn main() {
+    // the send and receiver for messages between our main (game) thread and our key thread
     let (tx, rx) = mpsc::channel();
-    let (exit_tx, exit_rx) = mpsc::channel();
 
-    let handle = thread::spawn(move || {
-        let mut stdout = std::io::stdout().into_raw_mode().unwrap();
-        stdout.flush().unwrap();
+    // use a separate thread for key input to prevent game code from interfering with input
+    // in hindsight, this was likely unnecessary, but it's too late and rust threads are cool anyway
+    #[allow(unused_variables)] let key_thread_handle = thread::spawn(move || {
+        // turns the current stdout into raw mode so we can do fancy things with drawing
+        let mut stdout = std::io::stdout().into_raw_mode()
+            .expect("Expected stdout to be a TTY capable of raw mode");
+        stdout.flush().expect("Expected to be able to write to stdout");
 
+        // stdout should still be around, right???
         write!(stdout, "{}", termion::cursor::Hide).unwrap();
 
+        // runs whenever we get a key, blocking until we exit it
         for c in stdin().keys() {
-            match exit_rx.try_recv() {
-                Ok(_) => { break; },
-                Err(_) => {}
-            }
-
-
-            match c.unwrap() {
-                Key::Char(' ') => {
+            match c.expect("Expected to have a key?? idk") {
+                Key::Char(' ') => {// pressed space
                     tx.send(KeyEvent::Jump).unwrap();
                 },
-                Key::Char('q') => {
+                Key::Char('q') => {// pressed q
+                    // tells the main thread to quit
+                    // note: this is not read immediately, rather the main thread reads it when it
+                    // gets around to it
                     tx.send(KeyEvent::Quit).unwrap();
+
+                    // exit this loop (and thus the main thread)
+                    break;
                 },
-                _ => {}
+                _ => {}// all other keys are irrelevant
             }
         }
 
-
+        // i mean at this point if stdout doesn't work that's your fault not mine
         stdout.flush().unwrap();
 
         write!(stdout, "{}", termion::cursor::Show).unwrap();
         stdout.suspend_raw_mode().unwrap();
     });
 
-    let (mut display, mut state, mut tick) = setup();
+    // a counter incremented every loop
+    // used for physics
+    let mut tick = 0;
 
+    // setup the display and state
+    let (mut display, mut state) = setup();
+
+    // game loop
     loop {
+        // start timing as soon as the loop starts
         let now = Instant::now();
 
+        // did our game thread say anything?
         match rx.try_recv() {
-            Ok(event) => {
+            Ok(event) => {// we got a KeyEvent!
                 match event {
-                    KeyEvent::Jump => {
+                    KeyEvent::Jump => {// jump key
                         if state.game_state == GameState::Playing {// if we're playing...
                             state.player.jump_left = state.jump_size;// ...start jumping...
                             state.player.fall_speed = 1f32; // ...and reset our fall speed
@@ -361,57 +406,70 @@ fn main() {
                             state.game_state = GameState::Death;
                         }
                     },
-                    KeyEvent::Quit => {
+                    KeyEvent::Quit => {// quit key
+                        // exit the loop
                         break;
                     }
                 };
             }
-            _ => {}
+            _ => {}// no? ok
         };
 
+        // clear terminal and reset cursor
         clear_terminal_and_reset_cursor();
 
+        // note: this does not draw the display to the terminal, rather it puts stuff in &mut display
         draw_display(&mut display, &mut state);
 
+        // main game state machine
         match state.game_state {
             GameState::Playing => {
                 physics(&mut state, tick);
             }
             GameState::Death => {
+                // do we respawn after death?
                 if AUTO_RESPAWN {
                     // get new display, state and tick variables
-                    let (display2, state2, tick2) = setup();
+                    let (display2, state2) = setup();
 
                     // replace the old ones with new ones
                     display = display2;
                     state = state2;
-                    tick = tick2;
                 } else {
+                    // display death message in the middle of the screen
                     display.iter_mut().for_each(|row| row.iter_mut().for_each(|pixel| *pixel = Pixel::Full));
 
-                    let message = "You Died.";
+                    let death_message = "You Died.";
 
-                    let start_x = ((WIDTH - message.len()) as f32 / 2 as f32).floor() as usize;
-                    let total = message.len();
+                    // where the you died message starts
+                    // (the difference of the width and message length, over 2)
+                    let start_x = ((WIDTH - death_message.len()) as f32 / 2 as f32).floor() as usize;
+                    let total = death_message.len();
                     for x in 0..total {
-                        display[x + start_x][HEIGHT / 2] = Pixel::Char(message.chars().collect::<Vec<char>>()[x]);
+                        // write the death message to the display
+                        display[x + start_x][HEIGHT / 2] = Pixel::Char(death_message.chars().collect::<Vec<char>>()[x]);
                     }
                 }
             }
             _ => {}
         }
 
-        println!("\rScore: {}\r", state.score);
+        // display our game score & other info at the top of the screen
+        println!("Flappy Rust \t Score: {} \t Press space to jump, q to quit\r", state.score);
+
+        // render the display
         render_display(&display);
 
-        // println!("\rThis frame took {:#?}", now.elapsed());
-        sleep(Duration::from_secs_f32(1f32 / (FPS as f32)).sub(now.elapsed()));
-        // println!("\rTotal frame time: {:#?}", now.elapsed());
+        // sleep the amount of time needed to target the given framerate
+        // this means that no matter how long the game loop took, the game won't update slower or faster
+        // as long as it finishes before when the next game loop should run
+        // if it doesn't, then i need to optimize my game or your computer is too slow
+        sleep(Duration::from_secs_f32(1f32 / (FPS as f32)).checked_sub(now.elapsed())
+            .expect("The game loop took too long to run. Consider purchasing a more powerful gaming computer."));
 
         tick += 1;
     }
 
-    println!("\rPress any key to continue to your terminal.\r");
-    exit_tx.send(()).unwrap();
-    handle.join().unwrap();
+    // clear the screen on close
+    clear_terminal_and_reset_cursor();
 }
